7695c0a41051c131b88f78590a49dc99550ceb57
Ryuji Oda <ryuji.oda@gmail.com>
2020-12-16 08:07:15 +0900

    practice A

---
+++ src/ABC081-A.kt
@@ -0,0 +1,6 @@
+//文字列に含まれる 1 の数を数える問題
+fun main(args: Array<String>) {
+  //  ラムダ式の引数が 1  つの場合、引数の宣言を省略して it を引数として使える
+  val a = readLine()!!.count { it == '1' }
+  println(a)
+}
--- src/ABC086-A.kt
+++ src/ABC086-A.kt
@@ -1,4 +1,5 @@
+//2 つの整数の積の偶奇を判定する問題です。
 fun main(args: Array<String>) {
-    val (a, b) = readLine()!!.split(" ").map(String::toInt)
-    println(if (a * b % 2 == 0) "Even" else "Odd")
+  val (a, b) = readLine()!!.split(" ").map(String::toInt)
+  println(if (a * b % 2 == 0) "Even" else "Odd")
 }
